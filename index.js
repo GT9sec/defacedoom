e43d5b608e181fb73189eccc289b43d763403106
gt9sec <167272959+GT9sec@users.noreply.github.com>
2025-01-31 20:18:31 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+var htmlContent = `
+<!DOCTYPE html>
+<title>Hacked By gt9 </title>
+<link rel="icon" href="" type="image/x-icon"/>
+<link rel="shortcut icon" href="" type="image/x-icon"/>
+<meta charset="UTF-8">
+<body bgcolor="black">
+  <style type="text/css">/* latin */ @font-face { font-family: 'Iceberg'; font-style: normal; font-weight: 400; src: local('Iceberg'), local('Iceberg-Regular'), url(https://fonts.gstatic.com/s/iceberg/v7/8QIJdijAiM7o-qnZiI8EqprnEO0.woff2) format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD; }</style> 
+  <style type="text/css">/* latin */ @font-face { font-family: 'Wallpoet'; font-style: normal; font-weight: 400; src: local('Wallpoet'), url(https://fonts.gstatic.com/s/wallpoet/v11/f0X10em2_8RnXVVdUObp58Tt868H.woff2) format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD; }</style>  
+  <style>
+    h1{
+      align-content: center;
+      font-size: 46px;
+    }
+    h4{
+      align-content: center;
+      font-size: 20px;
+    }
+    h3{
+      align-content: center;
+    }
+  </style>
+  
+  <center>
+    <font face="Iceberg" size="6" style="text-shadow: 0px 0px 20px green;" color="green">
+      <b>OWNED BY</b>
+    </font>
+    <font face="Iceberg" size="6" style="text-shadow: 0px 0px 20px red;" color="red">
+      <b>@GT9SEC</b>
+    </font>
+    <hr>
+    <iframe src="https://emupedia.net/emupedia-game-doom1/" width="1000" height="600" frameborder="0" allowfullscreen></iframe>
+    <hr>
+    <marquee style="font-size: 30px; font-weight: bold; color: green; font-family: 'Iceberg'">Um salve para meus manos!: Cyhp3r - Darui - Slash - Slow - zer0 - UprisingHK - 0xysha - Nano - psychoHKtech - archon</marquee>
+  </center>
+</body>
+</html>
+`;
+document.write(htmlContent);
