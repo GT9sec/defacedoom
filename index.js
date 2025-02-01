e689973a77df7c21d4e67f5b2d86f76d0f1de69a
gt9sec <167272959+GT9sec@users.noreply.github.com>
2025-01-31 23:26:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var htmlContent = `
 <link rel="icon" href="" type="image/x-icon"/>
 <link rel="shortcut icon" href="" type="image/x-icon"/>
 <meta charset="UTF-8">
-<body style="background-color: black;">
+<body style="background-color: black !important; margin: 0; padding: 0;">
   <style type="text/css">/* latin */ @font-face { font-family: 'Iceberg'; font-style: normal; font-weight: 400; src: local('Iceberg'), local('Iceberg-Regular'), url(https://fonts.gstatic.com/s/iceberg/v7/8QIJdijAiM7o-qnZiI8EqprnEO0.woff2) format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD; }</style> 
   <style type="text/css">/* latin */ @font-face { font-family: 'Wallpoet'; font-style: normal; font-weight: 400; src: local('Wallpoet'), url(https://fonts.gstatic.com/s/wallpoet/v11/f0X10em2_8RnXVVdUObp58Tt868H.woff2) format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD; }</style>  
   <style>
